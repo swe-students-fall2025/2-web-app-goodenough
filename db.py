ae1d29634f41af21bd3621c3589647e90e0c1906
Leo Li <shangchengli2022@163.com>
2025-10-17 16:24:42 -0400

    made some comments to the dp.py file

--- db.py
+++ db.py
@@ -6,10 +6,18 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DBNAME = os.getenv("MONGO_DBNAME")
 
-client = MongoClient(MONGO_URI)
-db = client[MONGO_DBNAME]
+_client = MongoClient(MONGO_URI)
+_db = _client[MONGO_DBNAME]
 
-users_collection = db['users']
-artworks_collection = db['artworks']
-comments_collection = db['comments']
-likes_collection = db['likes']
+users_collection = _db.get_collection['users']
+artworks_collection = _db.get_collection['artworks']
+comments_collection = _db.get_collection['comments']
+likes_collection = _db.get_collection['likes']
+
+# Define function to return database instance
+def get_db():
+    return _db
+
+# Define function to return the MongoClient instance
+def get_client():
+    return _client
